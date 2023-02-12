45a84088b2c633e5ca79dc6455370048fa305ca6
minhye9731 <jenney62639731@gmail.com>
2023-02-12 23:39:03 +0900

    feat: practice collectionview example

---
+++ FlexlayoutPractice/UI/Examples/CollectionViewExample/CollectionViewExampleView.swift
@@ -0,0 +1,70 @@
+//
+//  CollectionViewExampleView.swift
+//  FlexlayoutPractice
+//
+//  Created by 강민혜 on 2/12/23.
+//
+
+import UIKit
+
+class CollectionViewExampleView: UIView {
+    
+    fileprivate let collectionView: UICollectionView
+    fileprivate let flowLayout = UICollectionViewFlowLayout()
+    fileprivate let cellTemplate = HouseCell()
+    
+    fileprivate var houses: [House] = []
+    
+    init() {
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        
+        super.init(frame: .zero)
+        
+        flowLayout.minimumLineSpacing = 8
+        flowLayout.minimumInteritemSpacing = 0
+        
+        collectionView.backgroundColor = .white
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(HouseCell.self, forCellWithReuseIdentifier: HouseCell.reuseIdentifier)
+        addSubview(collectionView)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure(houses: [House]) {
+        self.houses = houses
+        collectionView.reloadData()
+    }
+    
+    func viewOrientationDidChange() {
+        flowLayout.invalidateLayout()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        collectionView.pin.vertically().horizontally(pin.safeArea)
+    }
+    
+}
+
+extension CollectionViewExampleView: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return houses.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HouseCell.reuseIdentifier, for: indexPath) as! HouseCell
+        cell.configure(house: houses[indexPath.row])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        cellTemplate.configure(house: houses[indexPath.row])
+        return cellTemplate.sizeThatFits(CGSize(width: collectionView.bounds.width, height: .greatestFiniteMagnitude))
+    }
+    
+}
